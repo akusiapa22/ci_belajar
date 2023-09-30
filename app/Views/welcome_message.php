37d0f035f81b6141198c6d4f3df108108c5d4418
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-09-30 13:15:29 +0900

    coba perubahan

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -219,7 +219,7 @@
     </div>
 
     <div class="heroe">
-
+        Selamat datang
         <h1>Welcome to akusiapa22 <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
